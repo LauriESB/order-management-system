b387cfe496b8bc9601e475d8a76be59a7bbdec12
llasantinelli <ich.lauri@gmail.com>
2024-09-16 17:54:20 -0300

    chore(init): create spring application

---
+++ backend/order-management-system/src/main/java/com/lauriesb/order_management/OrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lauriesb.order_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/order-management-system/src/test/java/com/lauriesb/order_management/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauriesb.order_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
